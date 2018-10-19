a33fb0c48561c3cc628e8e495ce7c857049d82e8
alexwin32 <alexwin32@mail.ru>
2018-10-19 19:02:06 +0300

    Privileges draft part2

--- NetboxTask/NetboxTask.cpp
+++ NetboxTask/NetboxTask.cpp
@@ -24,24 +24,6 @@ static std::wstring GetUserNameString()
 
     return username;
 }
-/*
-static BOOL IsElevated()
-{
-    BOOL fRet = FALSE;
-    HANDLE hToken = NULL;
-
-    if( OpenProcessToken( GetCurrentProcess( ),TOKEN_QUERY,&hToken ) ) {
-        TOKEN_ELEVATION Elevation;
-        DWORD cbSize = sizeof( TOKEN_ELEVATION );
-        if( GetTokenInformation( hToken, TokenElevation, &Elevation, sizeof( Elevation ), &cbSize ) ) {
-            fRet = Elevation.TokenIsElevated;
-        }
-    }
-    if( hToken ) {
-        CloseHandle( hToken );
-    }
-    return fRet;
-}
 
 BOOL IsRunAsAdmin()
 {
@@ -87,11 +69,10 @@ Cleanup:
 
     return fIsRunAsAdmin;
 }
-*/
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    /*
     if(!IsRunAsAdmin())
     {
         wchar_t szPath[MAX_PATH];
@@ -101,6 +82,7 @@ int _tmain(int argc, _TCHAR* argv[])
             SHELLEXECUTEINFO sei = { sizeof(sei) };
             sei.lpVerb = L"runas";
             sei.lpFile = szPath;
+            sei.lpParameters = NULL;
             sei.nShow = SW_SHOW;
             sei.fMask = SEE_MASK_NOCLOSEPROCESS;
             sei.hInstApp = NULL;
@@ -120,50 +102,6 @@ int _tmain(int argc, _TCHAR* argv[])
         }
     }
 
-    if(!IsElevated())
-    {
-        CREDUI_INFO cui;
-        TCHAR pszName[CREDUI_MAX_USERNAME_LENGTH+1];
-        TCHAR pszPwd[CREDUI_MAX_PASSWORD_LENGTH+1];
-        BOOL fSave;
-        DWORD dwErr;
-
-        cui.cbSize = sizeof(CREDUI_INFO);
-        cui.hwndParent = NULL;
-        cui.pszMessageText = TEXT("Enter administrator account information");
-        cui.pszCaptionText = TEXT("CredUITest");
-        cui.hbmBanner = NULL;
-        fSave = FALSE;
-        SecureZeroMemory(pszName, sizeof(pszName));
-        SecureZeroMemory(pszPwd, sizeof(pszPwd));
-        dwErr = CredUIPromptForCredentialsW(
-            &cui,                         // CREDUI_INFO structure
-            TEXT("TheServer"),            // Target for credentials
-                                          //   (usually a server)
-            NULL,                         // Reserved
-            0,                            // Reason
-            pszName,                      // User name
-            CREDUI_MAX_USERNAME_LENGTH+1, // Max number of char for user name
-            pszPwd,                       // Password
-            CREDUI_MAX_PASSWORD_LENGTH+1, // Max number of char for password
-            &fSave,                       // State of save check box
-            CREDUI_FLAGS_GENERIC_CREDENTIALS | CREDUI_FLAGS_ALWAYS_SHOW_UI | CREDUI_FLAGS_DO_NOT_PERSIST | CREDUI_FLAGS_EXPECT_CONFIRMATION );
-
-        if(!dwErr)
-        {
-
-            DWORD ret = CredUIConfirmCredentialsW(TEXT("TheServer"), true);
-            if(ret != NO_ERROR)
-            {
-                std::wstring errStr = Utils::GetErrorMessageString(ret);
-                int asd = 0;
-            }
-
-            SecureZeroMemory(pszName, sizeof(pszName));
-            SecureZeroMemory(pszPwd, sizeof(pszPwd));
-        }
-    }
-    */
     std::wstring installPath = L"C:\\Users\\" + GetUserNameString() + L"\\AppData\\Local\\TestFolder";
 
     bool install = false, uninstall = false;
